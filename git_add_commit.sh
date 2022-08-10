7e3d67a937471d1499be8f5c9eb8405a9bc422b3
hravemzdy <greatwork@hravemzdy.org>
2022-08-10 13:56:15 +0200

    Release 0.22.21.

--- git_add_commit.sh
+++ git_add_commit.sh
@@ -1,1 +1,2 @@
-﻿git add -A && git commit -m "Release $HMORG_VERSION."
+﻿export HMORG_VERSION=0.22.21
+git add -A && git commit -m "Release $HMORG_VERSION."
--- git_tag_push.sh
+++ git_tag_push.sh
@@ -1,1 +1,2 @@
-﻿git tag "$HMORG_VERSION" && git push origin --tags
+﻿export HMORG_VERSION=0.22.21
+git tag "$HMORG_VERSION" && git push origin --tags
